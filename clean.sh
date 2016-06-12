f245b6e7ded751bc2bba45cd77737530f21a2182
fhippolyte <fhippolyte@gmail.com>
2016-06-12 13:51:41 +0200

    Improve cleanning

--- clean.sh
+++ clean.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 
-echo "Remove containers..."
-docker rm -vf $(docker ps -a -q)
-echo
-
-echo "Remove images..."
-docker rmi $(docker images -a -q)
-echo
-
-
-echo
-echo "Remove temp files"
-rm -rf /tmp/mesos
-
+echo "Remove old containers..."
+docker rm -vf $(docker ps -a -q -f status=exited)
